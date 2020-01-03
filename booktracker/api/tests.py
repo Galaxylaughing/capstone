2d1edf7b28f5b821fbd3935a34e9efb6c12553f8
Sabrina Lowney <sabrina@galaxylaughing.com>
2020-01-02 17:29:14 -0800

    added tests for bookauthor model and tests for the book and bookauthor str methods

--- booktracker/api/tests.py
+++ booktracker/api/tests.py
@@ -31,3 +31,42 @@ class BookTest(TestCase):
         self.assertEqual(Book.objects.count(), expectedCount)
         # exists() returns True if the QuerySet contains any results
         self.assertTrue(filteredBooks.exists())
+
+    def test_book_str_method(self):
+        book_title = "A Good Book"
+
+        book = Book.objects.create(
+            title=book_title, user=self.user)
+        
+        self.assertEqual(str(book), book_title)
+
+
+class BookAuthorTests(TestCase):
+    """ Test module for the BookAuthor model """
+
+    def setUp(self):
+        # make the User a Book relies on
+        self.user = User.objects.create(
+            username="Fakey", password="password")
+        # make the Book a BookAuthor relies on
+        self.book = Book.objects.create(
+            title="Test Book", user=self.user)
+
+    def test_bookauthor_can_be_created(self):
+        expectedCount = BookAuthor.objects.count() + 1
+
+        BookAuthor.objects.create(
+            author_name="First Last", book=self.book)
+        filteredBookAuthors = BookAuthor.objects.filter(
+            author_name="First Last")
+
+        self.assertEqual(BookAuthor.objects.count(), expectedCount)
+        self.assertTrue(filteredBookAuthors.exists())
+
+    def test_bookauthor_str_method(self):
+        author_name = "M.K. Author"
+
+        book_author = BookAuthor.objects.create(
+            author_name=author_name, book=self.book)
+        
+        self.assertEqual(str(book_author), author_name)
