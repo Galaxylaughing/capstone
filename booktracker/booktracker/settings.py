b0e01ae2eda1e72b673b865d896d52944b663400
Sabrina Lowney <sabrina@galaxylaughing.com>
2020-01-12 15:18:42 -0800

    added separate installed apps section for heroku

--- booktracker/booktracker/settings.py
+++ booktracker/booktracker/settings.py
@@ -37,19 +37,32 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'userauth',
-    'api',
-]
+if os.environ.get("ENVIRONMENT") == "PROD":
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'rest_framework.authtoken',
+        'booktracker.userauth',
+        'booktracker.api',
+    ]
+else:
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'rest_framework.authtoken',
+        'userauth',
+        'api',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
