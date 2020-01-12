a02b6cb899c8205c8f0f4b13a30b9b424ad482e6
Sabrina Lowney <sabrina@galaxylaughing.com>
2020-01-12 11:01:29 -0800

    added static files to settings

--- booktracker/booktracker/settings.py
+++ booktracker/booktracker/settings.py
@@ -144,6 +144,16 @@ REST_FRAMEWORK = {
     ]
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # https://devcenter.heroku.com/articles/heroku-postgresql#connecting-with-django
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
